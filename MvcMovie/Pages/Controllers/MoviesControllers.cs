5a6cfbfdf4f42bc3a698da5f0ccfd20d84bc20ab
Admin <Admin@DESKTOP-B6294DK>
2025-06-03 21:56:59 +0300

    Добавление модели

---
+++ MvcMovie/Pages/Controllers/MoviesControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Data;
+
+public class MoviesController : Controller
+{
+    private readonly MvcMovieContext _context;
+
+    public MoviesController(MvcMovieContext context)
+    {
+        _context = context;
+    }
+}
