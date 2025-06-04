312e5cbc3735780feaaa012d84e52f7d767e7885
Utka12 <vania.yaskin2017@yandex.ru>
2025-06-04 12:20:12 +0300

    Изменения

---
+++ New/MvcMovie/MvcMovie/Views/Hello World/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcMovie.Pages.Views.Hello_World
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
